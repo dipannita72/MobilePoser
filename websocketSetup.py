96933c9348936abbb8a619b1a6a3c8ae9ce807d2
Dipannita Biswas <b.dipannita.72@gmail.com>
2026-03-06 00:56:13 -0500

    Update websocketSetup.py

--- websocketSetup.py
+++ websocketSetup.py
@@ -1,11 +1,12 @@
-!pip install fastapi uvicorn nest_asyncio pyngrok
+
 from pyngrok import ngrok
 
 # paste your token inside the quotes
 ngrok.set_auth_token("39vRAjrjmFQf9lvYlLpXzt34yA0_7zzbha2htKdzgnRaPdsdh")
 
-!pkill -f ngrok
 from pyngrok import ngrok
 ngrok.kill()
 
 public_url = ngrok.connect(8000)
+
+
